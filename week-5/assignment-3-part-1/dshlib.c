f865d21165d9d2979dfdda74780e1e605ae8b6e8
erw74 <erw74@tux4.cci.drexel.edu>
2025-02-09 14:20:39 -0500

    Added more comments and included a free mem statement

--- week-5/assignment-3-part-1/dshlib.c
+++ week-5/assignment-3-part-1/dshlib.c
@@ -95,7 +95,7 @@ int build_cmd_list(char *cmd_line, command_list_t *clist) {
 }
 
 
-/*  - Takes in command_t pointer (already allocated
+/*  - Takes in command_t pointer (already allocated)
 *   - Parses command and popoulates command_t
 *   - Error checks as it goes
 *   - Will return either 'OK' or 'ERR_CMD_OR_ARGS_TOO_BIG' 
@@ -198,6 +198,12 @@ void stripLTWhiteSpace(char* string) {
     }
 }
 
+
+// Given an input string and a pointer to a buffer as well as a delimiter
+// this function will copy the characters from the input string to the tokenBuffer
+// until the delimiter is reached. 
+// The inputString is modified in place so the chracters that were copied shifts the pointer over in 
+// memory to resume reading after the last occurrence of the delimiter. 
 int getTruncToken(char* inputString, char* tokenBuffer, char* delimiter) {
     size_t delimiterIndex = strcspn(inputString, delimiter);
     size_t cpyIndex = 0;
@@ -235,6 +241,13 @@ int getTruncToken(char* inputString, char* tokenBuffer, char* delimiter) {
     return 0; 
 }
 
+
+// implements extra credit dragon function
+// NOTE: the compression I used here was by
+// using a python script to generate a static
+// sequence of tokens, each with a pattern followed by how many times
+// that pattern repeats. Newlines are denoted 'n'.
+// this gets expanded and printed to stdout
 void printDragon() {
     char* compressedDragon[] = {" 72", "@1", "%4", " 23", "n", " 69", "%6", " 25", "n", " 68", "%6", " 26", "n", " 65", "%1", " 1", "%7", " 11", "@1", " 14", "n", " 64", "%10", " 8", "%7", " 11", "n", " 39", "%7", " 2", "%4", "@1", " 9", "%12", "@1", " 4", "%6", " 2", "@1", "%4", " 8", "n", " 34", "%22", " 6", "%28", " 10", "n", " 32", "%26", " 3", "%12", " 1", "%15", " 11", "n", " 31", "%29", " 1", "%19", " 5", "%3", " 12", "n", " 29", "%28", "@1", " 1", "@1", "%18", " 8", "%2", " 12", "n", " 28", "%33", " 1", "%22", " 16", "n", " 28", "%58", " 14", "n", " 28", "%50", "@1", "%6", "@1", " 14", "n", " 6", "%8", "@1", " 11", "%16", " 8", "%26", " 6", "%2", " 16", "n", " 4", "%13", " 9", "%2", "@1", "%12", " 11", "%11", " 1", "%12", " 6", "@1", "%1", " 16", "n", " 2", "%10", " 3", "%3", " 8", "%14", " 12", "%24", " 24", "n", " 1", "%9", " 7", "%1", " 9", "%13", " 13", "%12", "@1", "%11", " 23", "n", "%9", "@1", " 16", "%1", " 1", "%13", " 12", "@1", "%25", " 21", "n", "%8", "@1", " 17", "%2", "@1", "%12", " 12", "@1", "%28", " 18", "n", "%7", "@1", " 19", "%15", " 11", "%33", " 14", "n", "%10", " 18", "%15", " 10", "%35", " 6", "%4", " 2", "n", "%9", "@1", " 19", "@1", "%14", " 9", "%12", "@1", " 1", "%4", " 1", "%17", " 3", "%8", "n", "%10", " 18", "%17", " 8", "%13", " 6", "%18", " 1", "%9", "n", "%9", "@1", "%2", "@1", " 16", "%16", "@1", " 7", "%14", " 5", "%24", " 2", "%2", "n", " 1", "%10", " 18", "%1", " 1", "%14", "@1", " 8", "%14", " 3", "%26", " 1", "%2", "n", " 2", "%12", " 2", "@1", " 11", "%18", " 8", "%40", " 2", "%3", " 1", "n", " 3", "%13", " 1", "%2", " 2", "%1", " 2", "%1", "@1", " 1", "%18", " 10", "%37", " 4", "%3", " 1", "n", " 4", "%18", " 1", "%22", " 11", "@1", "%31", " 4", "%7", " 1", "n", " 5", "%39", " 14", "%28", " 8", "%3", " 3", "n", " 6", "@1", "%35", " 18", "%25", " 15", "n", " 8", "%32", " 22", "%19", " 2", "%7", " 10", "n", " 11", "%26", " 27", "%15", " 2", "@1", "%9", " 9", "n", " 14", "%20", " 11", "@1", "%1", "@1", "%1", " 18", "@1", "%18", " 3", "%3", " 8", "n", " 18", "%15", " 8", "%10", " 20", "%15", " 4", "%1", " 9", "n", " 16", "%36", " 22", "%14", " 12", "n", " 16", "%26", " 2", "%4", " 1", "%3", " 22", "%10", " 2", "%3", "@1", " 10", "n", " 21", "%19", " 1", "%6", " 1", "%2", " 26", "%13", "@1", " 10", "n", " 81", "%7", "@1", " 8", "n"};
     for (int tokenIndex = 0; tokenIndex < 362; tokenIndex++) {
@@ -277,6 +290,7 @@ void printDragon() {
         }
 
         free(pattern);
+        free(repeat);
     }
 }
 
